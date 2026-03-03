8bde85a2375844b25c077c99deb30f4c8bd9bcea
GHOUSHENISHA BEGAM J <Digho0513@gmail.com>
2026-03-03 23:42:17 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,18 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open('trustnet-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => caches.delete(cache))
+      );
     })
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
